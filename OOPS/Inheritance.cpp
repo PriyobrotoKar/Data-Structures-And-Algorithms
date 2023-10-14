7fb5e344f08a4b4d0b3e765c0ff8f480b2d4e9b8
Priyobroto Kar <priyobrotokar@gmail.com>
2023-10-14 11:14:14 +0530

    Added inheritance from L43

---
+++ OOPS/Inheritance.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+    string name;
+    int age, roll, std;
+
+protected:
+    Student(string n, int a, int r, int s)
+    {
+        name = n;
+        age = a;
+        roll = r;
+        std = s;
+    }
+    void display()
+    {
+        cout << name << " " << age << " " << roll << " " << std << endl;
+    }
+};
+
+class Grade : protected Student
+{
+    int phy, chem, maths;
+
+public:
+    Grade(string n, int a, int r, int s, int p, int c, int m) : Student(n, a, r, s)
+    {
+        phy = p;
+        chem = c;
+        maths = m;
+    }
+    void display()
+    {
+        Student::display();
+        cout << "Physics marks:" << phy << " Chemistry Marks:" << chem << endl;
+    }
+};
+
+int main()
+{
+    Grade obj("Ramesh", 13, 45, 6, 89, 77, 95);
+    obj.display();
+}
